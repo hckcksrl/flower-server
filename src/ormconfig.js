6d85cea4316af3d7acb08ae973620a659c1eb204
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-03-16 22:59:06 +0900

    Database OrmCongih

--- src/ormconfig.js
+++ src/ormconfig.js
@@ -1,10 +1,21 @@
+// export const connectionOptions = {
+//   type: "mysql",
+//   host: "localhost",
+//   port: 3306,
+//   username: "root",
+//   password: "1111",
+//   database: "flower",
+//   synchronize: true,
+//   entities: [__dirname + "/entity/*.js"]
+// };
+
 export const connectionOptions = {
-  type: "mysql",
+  type: "postgres",
+  database: "flowers",
   host: "localhost",
-  port: 3306,
-  username: "root",
+  port: 5432,
+  username: "hckcksrl",
   password: "1111",
-  database: "flower",
   synchronize: true,
   entities: [__dirname + "/entity/*.js"]
 };
