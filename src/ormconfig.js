ad2894e422251cb97b7ab046ae1870c57357c6a1
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-03-19 20:11:42 +0900

    Database Config

--- src/ormconfig.js
+++ src/ormconfig.js
@@ -8,14 +8,16 @@
 //   synchronize: true,
 //   entities: [__dirname + "/entity/*.js"]
 // };
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
 
 export const connectionOptions = {
   type: "postgres",
   database: "flowers",
-  host: "localhost",
-  port: 5432,
-  username: "hckcksrl",
-  password: "1111",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   synchronize: true,
   entities: [__dirname + "/entity/*.js"]
 };
